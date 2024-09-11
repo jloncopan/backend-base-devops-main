0ac2093abfb33b6350c8a16a3acd0d738ebe6eff
José Loncopan Galaz <jloncopangalaz@gmail.com>
2024-09-10 23:20:18 -0300

    Pruebas sonarqube

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -1,4 +1,5 @@
 import { describe, test, expect } from "@jest/globals";
+import { esPrimo} from "../src/numeros.js";
 import app from "../src/server.js";
 import request from "supertest";
 import { configuration } from "../src/config.js";
@@ -19,6 +20,13 @@ describe("Test Suite App", () => {
 
     test("endpoint /primo", () => {
         expect(1 + 1).toBe(2);
+    });
+
+    describe('esPrimo', () => {
+        // Caso 1: Número menor que 2
+        test('debería devolver false para números menores que 2', () => {
+            expect(esPrimo(0)).toBe(false);
+            expect(esPrimo(1)).toBe(false);
     });
 
     test("test de endpoint /", async () => {
